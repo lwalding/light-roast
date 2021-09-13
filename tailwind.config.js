9a15bbc2aec9fe8facd5e362c31495e7c0776d14
Luke Walding <luke.w@me.com>
2021-09-13 13:50:08 +1000

    MVP release

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,24 @@ module.exports = {
   theme: {
     fontFamily: {
        'display': ['"Archer A"', '"Archer B"'],
-       'subhead': ['"Ringside Regular A"', '"Ringside Regular B"'],
        'body': ['"Ringside Regular A"', '"Ringside Regular B"'],
-      }
+      },
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },  
   },
   variants: {},
   plugins: [
